c637417703b88de07768d80df0ac806fa8272a83
Benjamin Kearns <rapiidz1@gmail.com>
2018-12-03 16:37:18 +0000

    first commit

---
+++ src/main/java/http/localhost0/SpringHelloWorld/SpringHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package http.localhost0.SpringHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloWorldApplication.class, args);
+	}
+}
